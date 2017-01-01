024a92a5b5d052e22b85f09829e0060a97ec9513
arthur <arthurschiro@gmail.com>
2016-12-31 18:38:08 -0800

    enabled serial ctl

--- temp_ctl/temp_ctl.ino
+++ temp_ctl/temp_ctl.ino
@@ -1,6 +1,11 @@
 /*
-//temp_ctl
+  //temp_ctl
 */
+
+#include <string.h>
+
+
+
 #define AREF 5000
 #define ADC_MAX 1023
 #define ADC2MV AREF/ADC_MAX
@@ -18,14 +23,25 @@
 #define SETPOINT_GROWTH .1
 #define SETPOINT_DECAY .1
 
-#define CONT_HYST 2500
-#define MIN_T 60000
-#define MAX_T 90000
+#define CONT_HYST 1500
+#define MIN_T 50000
+#define MAX_T 80000
 #define CONVERSION_SLOPE ((MAX_T-MIN_T)/AREF)
 
-unsigned long temp_setpoint=0;
-unsigned long temp_val=0;
+#define POT_CTL 0
+#define SERIAL_CTL 1
+
+unsigned long temp_hyst=CONT_HYST;
+unsigned long pot_temp_setpoint = 0;
+unsigned long serial_temp_setpoint = 0;
+unsigned long temp_val = 0;
 char print_buff[100];
+#define INPUT_BUFF_LENGTH 100
+char input_buff[INPUT_BUFF_LENGTH];
+char input_buff_ctr = 0;
+
+char setpoint_ctl_state = POT_CTL;
+unsigned long last_pot_temp_setpoint = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -34,11 +50,11 @@ void setup() {
   pinMode(OFF_PIN, OUTPUT);
   pinMode(ON_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
-  
+
   digitalWrite(OFF_PIN, LOW);
   digitalWrite(ON_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
-  
+
 }
 
 // the loop routine runs over and over again forever:
@@ -46,9 +62,77 @@ void loop() {
   while (1)
   {
     handle_state_machine();
+    handle_serial_ctl();
     delay(1);
   }
 }
+
+#define SET_TEMP_CMD "temp "
+#define SET_HYST_CMD "hyst "
+
+void handle_serial_ctl(void)
+{
+  char c;
+  char *p;
+  long num;
+
+  if (Serial.available() > 0)
+  {
+    c = Serial.read();
+    if (c == '\n' | c == '\r') //command receive complete
+    {
+      input_buff[input_buff_ctr] = '\0';
+      input_buff_ctr = 0;
+        
+      if (strncmp(input_buff, SET_TEMP_CMD, strlen(SET_TEMP_CMD)) == 0)
+      {
+        //get the number
+        num=str2long(input_buff + strlen(SET_TEMP_CMD));
+        serial_temp_setpoint=num;
+        last_pot_temp_setpoint=pot_temp_setpoint;
+        setpoint_ctl_state=SERIAL_CTL;
+        
+        sprintf(print_buff, "new setpoint/10 %u \n\r", (unsigned int)(num/10));
+        Serial.print(print_buff);
+      }
+      else if (strncmp(input_buff, SET_HYST_CMD, strlen(SET_HYST_CMD)) == 0)
+      {
+        //get the number
+        num=str2long(input_buff + strlen(SET_TEMP_CMD));
+        temp_hyst=num;
+        
+        sprintf(print_buff, "new hist value %u \n\r", (unsigned int)(num/10));
+        Serial.print(print_buff);
+      }
+    }
+    else
+    {
+      input_buff[input_buff_ctr] = c;
+      input_buff_ctr = ((input_buff_ctr + 1) >= INPUT_BUFF_LENGTH) ? 0 : ++input_buff_ctr;
+    }
+  }
+}
+
+long str2long(char * input)
+{
+  char num_dig=0,i;
+  long val=0, multiplier=1;
+
+  //find number of digits
+  for(i=0;input[i]>='0' && input[i]<='9';++i)
+    num_dig=num_dig+1;
+        
+  //compute value
+  for(i=0;i<num_dig;++i)
+  {
+    val=val+multiplier*(input[num_dig-1-i]-'0');
+    multiplier=multiplier*10;
+  }
+  
+  return val;
+}
+
+
 
 #define OFF 0
 #define ON 1
@@ -66,154 +150,188 @@ void loop() {
 
 void handle_state_machine(void)
 {
-  static unsigned long state_change_timer=0;
-  static unsigned long read_values_timer=0;
-  static unsigned long action_timer=0;
-  static unsigned long transmit_timer=0;
-  static unsigned long saftey_timer=0;
-  static unsigned char action_counter=0;
-  static unsigned char state=OFF;
-  static unsigned char action_state=IDLE_STATE;
-  
+  static unsigned long state_change_timer = 0;
+  static unsigned long read_values_timer = 0;
+  static unsigned long action_timer = 0;
+  static unsigned long transmit_timer = 0;
+  static unsigned long saftey_timer = 0;
+  static unsigned char action_counter = 0;
+  static unsigned char state = OFF;
+  static unsigned char action_state = IDLE_STATE;
+
+  unsigned long temp_setpoint;
 
   //
-  if(elapsed_time(read_values_timer)>READ_VALUES_PERIOD)
+  if (elapsed_time(read_values_timer) > READ_VALUES_PERIOD)
   {
-    read_values_timer=millis();
+    read_values_timer = millis();
     refresh_setpoint_and_temp();
   }
 
   //
-  if(elapsed_time(saftey_timer)>SAFTEY_TIMER_PERIOD)
+  if (elapsed_time(saftey_timer) > SAFTEY_TIMER_PERIOD)
   {
-    saftey_timer=millis();
-    action_counter=NUM_ACTIONS;
+    saftey_timer = millis();
+    action_counter = NUM_ACTIONS;
   }
-  
-  //
-  if(elapsed_time(state_change_timer)>ACTION_TIMER_PERIOD)
-  {
-    state_change_timer=millis();
 
-    switch(state)
+  //
+  if (elapsed_time(state_change_timer) > ACTION_TIMER_PERIOD)
+  {
+    state_change_timer = millis();
+
+    if(setpoint_ctl_state==POT_CTL)
+      temp_setpoint=pot_temp_setpoint;
+    else
+      temp_setpoint=serial_temp_setpoint;
+
+
+      sprintf(print_buff,"%u %u %u\r\n",
+                (unsigned int)(temp_val/10),
+                (unsigned int)(temp_setpoint/10),
+                (unsigned int)((temp_setpoint-temp_hyst)/10));
+      Serial.print(print_buff);
+    
+    switch (state)
     {
       case OFF:
-        if(temp_val<temp_setpoint)
+        if (temp_val < (temp_setpoint-temp_hyst))
         {
-          state=ON;
+          state = ON;
           digitalWrite(LED_PIN, HIGH);
-          action_counter=NUM_ACTIONS;
-          saftey_timer=millis();
+          action_counter = NUM_ACTIONS;
+          saftey_timer = millis();
         }
-      break;
+        break;
       case ON:
-        if(temp_val>(temp_setpoint+CONT_HYST))
+        if (temp_val > temp_setpoint)
         {
-          state=OFF;
+          state = OFF;
           digitalWrite(LED_PIN, LOW);
-          action_counter=NUM_ACTIONS;
-          saftey_timer=millis();
+          action_counter = NUM_ACTIONS;
+          saftey_timer = millis();
         }
-      break;      
+        break;
     }
   }
 
   //
-  if(elapsed_time(action_timer)>ACTION_TIMER_PERIOD)
+  if (elapsed_time(action_timer) > ACTION_TIMER_PERIOD)
   {
-    action_timer=millis();
-    
-    switch(action_state)
+    action_timer = millis();
+
+    switch (action_state)
     {
       case IDLE_STATE:
-        if(action_counter)
+        if (action_counter)
         {
-          if(state==ON)
+          if (state == ON)
           {
             on_action();
-            transmit_timer=millis();
-            action_state=ON_ACTIVE;
+            transmit_timer = millis();
+            action_state = ON_ACTIVE;
           }
           else //state==OFF
           {
             off_action();
-            transmit_timer=millis();
-            action_state=OFF_ACTIVE;
+            transmit_timer = millis();
+            action_state = OFF_ACTIVE;
           }
         }
-      break;
+        break;
       case ON_ACTIVE:
-        if(elapsed_time(transmit_timer)>TRANSMIT_DURATION)
+        if (elapsed_time(transmit_timer) > TRANSMIT_DURATION)
         {
-            no_action();
-            transmit_timer=millis();
-            action_state=NULL_ACTIVE;
+          no_action();
+          transmit_timer = millis();
+          action_state = NULL_ACTIVE;
         }
-      break; 
+        break;
       case OFF_ACTIVE:
-        if(elapsed_time(transmit_timer)>TRANSMIT_DURATION)
+        if (elapsed_time(transmit_timer) > TRANSMIT_DURATION)
         {
-            no_action();
-            transmit_timer=millis();
-            action_state=NULL_ACTIVE;
+          no_action();
+          transmit_timer = millis();
+          action_state = NULL_ACTIVE;
         }
-      break; 
+        break;
       case NULL_ACTIVE:
-        if(elapsed_time(transmit_timer)>TRANSMIT_DURATION)
+        if (elapsed_time(transmit_timer) > TRANSMIT_DURATION)
         {
-            no_action();
-            if(action_counter>0)
-              action_counter--;
-              
-            action_state=IDLE_STATE;
+          no_action();
+          if (action_counter > 0)
+            action_counter--;
+
+          action_state = IDLE_STATE;
         }
-      break;      
+        break;
     }
   }
-  
+
 }
 
 void refresh_setpoint_and_temp(void)
 {
-  unsigned long setpoint_now,temp_now;
+  unsigned long setpoint_now, temp_now,diff;
   unsigned int offset;
-  
-  //read setpoint value  
-  setpoint_now=((unsigned long)(((double)read_adc_mv(POT_PORT))*CONVERSION_SLOPE))+MIN_T;
-  
-  //if calibration needs to be done
-  if (temp_setpoint == 0)
-    temp_setpoint = setpoint_now;
-  
-  //implement exponential average
-  if (setpoint_now > temp_setpoint)
-    temp_setpoint = temp_setpoint + ((unsigned long)(((double)(setpoint_now - temp_setpoint)) * SETPOINT_GROWTH));
-  else
-    temp_setpoint = temp_setpoint - ((unsigned long)(((double)(temp_setpoint - setpoint_now)) * SETPOINT_DECAY));
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+  //read setpoint value
+  setpoint_now = ((unsigned long)(((double)read_adc_mv(POT_PORT)) * CONVERSION_SLOPE)) + MIN_T;
+
+  //if calibration needs to be done
+  if (pot_temp_setpoint == 0)
+    pot_temp_setpoint = setpoint_now;
+
+  //implement exponential average
+  if (setpoint_now > pot_temp_setpoint)
+    pot_temp_setpoint = pot_temp_setpoint + ((unsigned long)(((double)(setpoint_now - pot_temp_setpoint)) * SETPOINT_GROWTH));
+  else
+    pot_temp_setpoint = pot_temp_setpoint - ((unsigned long)(((double)(pot_temp_setpoint - setpoint_now)) * SETPOINT_DECAY));
+
+  if(setpoint_ctl_state==SERIAL_CTL)
+  {
+    if(pot_temp_setpoint>last_pot_temp_setpoint)
+      diff=pot_temp_setpoint-last_pot_temp_setpoint;
+    else
+      diff=last_pot_temp_setpoint-pot_temp_setpoint;
     
+    if(diff>1000)
+      setpoint_ctl_state=POT_CTL;
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
   //read temp value
   temp_now = read_temp_F(TEMP_PORT);
-  
+
   //if calibration needs to be done
   if (temp_val == 0)
     temp_val = temp_now;
-  
+
   //implement exponential averager
   if (temp_now > temp_val)
     temp_val = temp_val + ((unsigned long)(((double)(temp_now - temp_val)) * AVG_GROWTH));
   else
     temp_val = temp_val - ((unsigned long)(((double)(temp_val - temp_now)) * AVG_DECAY));
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-    offset=(temp_val/10)-400;
-    sprintf(print_buff,"%u %u ",(unsigned int)(digitalRead(ON_PIN)*200 +offset),(unsigned int)(digitalRead(OFF_PIN)*200 +offset));
-    Serial.print(print_buff);
-    sprintf(print_buff,"%u %u %u ",(unsigned int)(setpoint_now/10),(unsigned int)(temp_setpoint/10),(unsigned int)((temp_setpoint+CONT_HYST)/10));
-    Serial.print(print_buff);
-    sprintf(print_buff,"%u %u\r\n",(unsigned int)(temp_now/10),(unsigned int)(temp_val/10));
-    Serial.print(print_buff);
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+//if(setpoint_ctl_state==POT_CTL)
+//{
+//      sprintf(print_buff,"%u %u %u ",
+//            (unsigned int)(setpoint_now/10),
+//            (unsigned int)(pot_temp_setpoint/10) +digitalRead(ON_PIN)*200-digitalRead(OFF_PIN)*200,
+//            (unsigned int)((pot_temp_setpoint-temp_hyst)/10));
+//}
+//else
+//{
+//      sprintf(print_buff,"%u %u %u ",
+//            (unsigned int)(serial_temp_setpoint/10) +digitalRead(ON_PIN)*200-digitalRead(OFF_PIN)*200,
+//            (unsigned int)((serial_temp_setpoint-temp_hyst)/10));
+//}
+//      Serial.print(print_buff);
+//  
+//      sprintf(print_buff,"%u %u\r\n",(unsigned int)(temp_now/10),(unsigned int)(temp_val/10));
+//      Serial.print(print_buff);
 
 }
 
@@ -222,16 +340,16 @@ void refresh_setpoint_and_temp(void)
 void clear_plot(unsigned int val)
 {
   unsigned int count;
-  for(count=0;count<501;count++)
+  for (count = 0; count < 501; count++)
     Serial.println(val);
 }
 
 unsigned long elapsed_time(unsigned long then)
 {
   unsigned long now;
-  now=millis();
-  if(now>=then)
-    return now-then;
+  now = millis();
+  if (now >= then)
+    return now - then;
   else
     return now;
 }
@@ -287,6 +405,6 @@ void no_action(void)
 {
   digitalWrite(OFF_PIN, LOW);
   digitalWrite(ON_PIN, LOW);
-  
+
 }
 
